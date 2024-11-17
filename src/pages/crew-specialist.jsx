1a981d0adc4ed3b841ebf5d4e0a00be20277b280
unknown <ricardodonado1105@gmail.com>
2024-11-17 11:32:25 -0500

    importing front-end mentor files

---
+++ src/pages/crew-specialist.jsx
@@ -0,0 +1,19 @@
+export default function CrewSpecialist() {
+  return (
+<body>
+
+  00 Home
+  01 Destination
+  02 Crew
+  03 Technology
+
+  02 Meet your crew
+
+  Mission Specialist
+  Mark Shuttleworth
+
+  Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
+  the Linux-based Ubuntu operating system. Shuttleworth became the first South 
+  African to travel to space as a space tourist.
+</body>
+)}
