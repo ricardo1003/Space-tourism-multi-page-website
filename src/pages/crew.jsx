97b1f5b63b6e408a5f590e7a6745dddda5226fd8
unknown <ricardodonado1105@gmail.com>
2024-12-09 14:06:59 -0500

    crew mobile design done

--- src/pages/crew.jsx
+++ src/pages/crew.jsx
@@ -30,19 +30,19 @@ export default function CrewCommander({ crew, cleanPath, pageTransitions, isExit
       exit={{ opacity: pageTransitions.exit }}
       transition={{ duration: pageTransitions.transition }}
     >
-      <h1 className="font-Barlow text-[28px] tracking-[4px] self-start mt-[4vh] mb-[6vh] max-[769px]:text-[24px] max-[769px]:tracking-0">
+      <h1 className="flex font-Barlow text-[28px] tracking-[4px] self-start mt-[4vh] mb-[6vh] max-[769px]:text-[24px] max-[769px]:tracking-0 max-[458px]:text-[16px] max-[458px]:self-center max-[458px]:gap-6">
         <b className="tracking-[4.72px] text-white/25">02</b> MEET YOUR CREW
       </h1>
       <section className="flex justify-between w-full max-[769px]:flex-col max-[769px]:items-center text-center h-full">
         <div className="flex flex-col max-w-[539px]">
           <div className="my-auto">
-            <h2 className="flex flex-col uppercase font-Bellefair text-[56px] gap-0 max-[769px]:text-[40px] max-[769px]:mb-[24px]">
-              <span className="text-[hsla(0,0%,100%,0.5042)] text-[32px] max-[769px]:text-[24px]">
+            <h2 className="flex flex-col uppercase font-Bellefair text-[56px] gap-0 max-[769px]:text-[40px] max-[769px]:mb-[24px] max-[458px]:text-2xl">
+              <span className="text-[hsla(0,0%,100%,0.5042)] text-[32px] max-[769px]:text-[24px] max-[458px]:text-lg">
                 {crew.role}
               </span>
               {crew.name}
             </h2>
-            <p className="font-Barlow text-[18px] leading-[1.8em] text-Blue-300 max-[769px]:text-[16px]">
+            <p className="font-Barlow text-[18px] leading-[1.8em] text-Blue-300 max-[769px]:text-[16px] max-[458px]:text-[15px]">
               {crew.bio}
             </p>
           </div>
@@ -63,12 +63,12 @@ export default function CrewCommander({ crew, cleanPath, pageTransitions, isExit
             </ol>
           </nav>
         </div>
-        <picture className="max-w-full max-h-[60vh] h-[100%] relative max-[769px]:max-h-[50vh] mt-auto z-10">
+        <picture className="flex items-end max-w-full max-h-[60vh] h-[100%] relative max-[769px]:max-h-[50vh] mt-auto z-10 max-[458px]:mb-2">
           <div className="absolute block w-full h-[25%] bottom-0 bg-gradient-to-t from-Blue-900 from-10% z-10"></div>
           <img
             src={cleanPath(crew.images.png)}
             alt="Douglas"
-            className="max-w-full max-h-full"
+            className="max-w-full max-h-full block"
           />
         </picture>
       </section>
